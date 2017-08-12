0cc7a4e634c385dda75e47761f3e392e4f8f95d0
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-08-12 10:57:22 +0300

    Added release downloads

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function a(){
+    $.get('https://api.github.com/repos/KaMeHb-UA/LeNode/releases/latest', function(data, status){
+        if (status != 'success') a(); else {
+            $('a[rel="download"]').attr('href', data.assets[0].browser_download_url);
+            $('#latest-version').html(data.tag_name);
+            $('*[rel="release-description"]').html(data.body.replace(/#* /,''));
+        }
+    }, 'json');
+})();
