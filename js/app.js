428ac3e023b490009ea4faecbad0a636bd5758f7
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-08-21 14:38:39 +0300

    Little fix

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
         if (status != 'success') a(); else {
             $('a[rel="download"]').attr('href', data.assets[0].browser_download_url);
             $('#latest-version').html(data.tag_name);
-            $('*[rel="release-description"]').html(data.body.replace(/#* /,''));
+            $('*[rel="release-description"]').html(data.name);
         }
     }, 'json');
 })();
